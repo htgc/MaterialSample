b7c81bdf08c5016af05f602fabdf12f321ccfe32
Hidemasa Togashi <togachiro@gmail.com>
2016-01-21 19:02:17 +0800

    Commit NavigationView

--- app/src/main/java/com/example/htgc/materialsample/MainActivity.java
+++ app/src/main/java/com/example/htgc/materialsample/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.htgc.materialsample;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+//        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
     }
 
     @Override
@@ -42,9 +48,12 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
