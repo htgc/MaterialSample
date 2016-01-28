0174cd52d5971e4867c6ded0bd5ac6b89969a109
Hidemasa Togashi <togachiro@gmail.com>
2016-01-28 11:32:53 +0900

    Add for cardview

---
+++ app/src/main/java/com/example/htgc/materialsample/adapter/DesignDemoRecyclerAdapter.java
@@ -0,0 +1,47 @@
+package com.example.htgc.materialsample.adapter;
+
+import java.util.List;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.htgc.materialsample.R;
+
+/**
+ * Created by htgc on 1/21/16.
+ */
+public class DesignDemoRecyclerAdapter extends RecyclerView.Adapter<DesignDemoRecyclerAdapter.ViewHolder> {
+    private List<String> mItems;
+
+    public DesignDemoRecyclerAdapter(List<String> items) {
+        mItems = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_row, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        String item = mItems.get(i);
+        viewHolder.mTextView.setText(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView mTextView;
+        ViewHolder(View v) {
+            super(v);
+            mTextView = (TextView)v.findViewById(R.id.list_item);
+        }
+    }
+}
